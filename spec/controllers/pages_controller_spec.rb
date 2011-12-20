f1c7b5799419190cacd462de227f24ed4be28e3e
Malcolm <featonby@amazon.com>
2011-12-21 00:01:17 +0200

    Added static pages and stylesheets

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,18 +2,42 @@ require 'spec_helper'
 
 describe PagesController do
 
+  # Flag indicates that the views should also be tested, not view the controller
+  render_views
+
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
+    end
+    it "should have the correct title" do
+      get 'home'
+      response.should have_selector("title",
+                      :content => "Ruby on Rails Tutorial Sample App | Home")
     end
   end
 
   describe "GET 'contact'" do
     it "should be successful" do
       get 'contact'
-      response.should be_success
+      response.should be_success 
+    end
+    it "should return the correct title" do
+      get 'contact'
+      response.should have_selector("title",
+                      :content => "Ruby on Rails Tutorial Sample App | Contact")
     end
   end
-
+  
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success 
+    end
+    it "should return the correct title" do
+      get 'about'
+      response.should have_selector("title",
+                      :content => "Ruby on Rails Tutorial Sample App | About")
+    end
+  end
 end
