40b88dc8f36028e2c98c23349f70e6a511503ebb
Malcolm <featonby@amazon.com>
2012-01-11 06:44:13 +0200

    Added integration tests for the sign up

---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,56 @@
+require 'spec_helper'
+
+describe "Users" do
+
+  describe "GET /users" do
+
+    it "should return a valid page" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      get users_path
+      response.status.should be(200)
+    end
+
+    it "should be able to capture a valid user" do
+      visit signup_path
+      fill_in "Name", :with => "TestName"
+      fill_in "Email", :with => "email@address.com"
+      fill_in "Password", :with => "password"
+      fill_in "Password Confirmation", :with => "password"
+      click_button
+
+      response.status.should be(200)
+      response.should render_template("users/new")
+      response.should have_selector("div.flash.success")
+    end
+
+    it "should report errors correctly" do
+      visit signup_path
+      fill_in "Name", :with => ""
+      fill_in "Email", :with => "@address.com"
+      fill_in "Password", :with => ""
+      fill_in "Password Confirmation", :with => ""
+      click_button
+
+      response.status.should be(200)
+      response.should render_template("users/new")
+      response.should have_selector("div#error_explanation")
+
+    end
+
+    it "should capture the new user in the database" do
+      lambda do
+        visit signup_path
+        fill_in "Name", :with => "TestingTesting"
+        fill_in "Email", :with => "email2@address.com"
+        fill_in "Password", :with => "password"
+        fill_in "Password Confirmation", :with => "password"
+        click_button
+
+        response.status.should be(200)
+        response.should render_template("users/new")
+        response.should have_selector("div.flash.success")
+      end.should change(User, :count).by(1)
+    end
+    
+  end
+end
